6718190e607a2cf43e8e3de7e5f4997516a5c5e4
Ian Pinili <gianpinili@gmail.com>
2024-02-21 20:43:05 -0800

    add header styling on tablet/desktop

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import './Header.scss';
 import mainLogo from '../../assets/logo/BrainFlix-logo.svg'
 import headerIcon from '../../assets/images/Mohan-muruge.jpg'
+import searchIcon from '../../assets/icons/search.svg'
 
 function Header () {
  return (
@@ -8,15 +9,18 @@ function Header () {
    <div className="header__logo">
     <img src={mainLogo} alt="BrainFlix Logo" />
    </div>
+
+   <div className="header__right">
    <div className="header__nav">
-     <input className="header__input" type="text" placeholder='Search' />
+    {/* <img className="header__search" src={searchIcon} alt="" /> */}
+     <input className="header__input" type="text" placeholder= 'Search' />
      <img className="header-icon__mobile" src={headerIcon} alt="" />
      </div>
 
    <div className="header__buttons">
     <button className="header__button">Upload</button>
     <img className="header-icon__desktop" src={headerIcon} alt="" />
-
+   </div>
    </div>
   </header>
  )
