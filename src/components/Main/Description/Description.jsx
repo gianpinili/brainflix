d638c0d9868ae2eda296a27642862b3d2e556c58
Ian Pinili <gianpinili@gmail.com>
2024-02-21 21:36:30 -0800

    add main description mobile style

--- src/components/Main/Description/Description.jsx
+++ src/components/Main/Description/Description.jsx
@@ -1,20 +1,30 @@
+import './Description.scss';
+import viewsIcon from '../../../assets/icons/views.svg';
+import likeIcon from '../../../assets/icons/likes.svg';
+
 function Description ({props}) {
  
  return (
   <>
-  <h2 className="main__title">{props[0].title}</h2>
+  <h2 className="main__title">Tech Trends:   The Future of Artificial Intelligence</h2>
 
 <section className="main__info">
  <div className="main__artist">
-  <p className="main__channel">By {props[0].channel}</p>
-  <p className="main__date">Date in Unix {props[0].timestamp}</p>
+  <p className="main__channel">By Aiden Thompson</p>
+  <p className="main__date">8/8/2023</p>
  </div>
  <div className="main__stats">
-  <p className="main__views">icon -{props[0].views}</p>
-  <p className="main__likes">icon -{props[0].likes}</p>
+  <div className="main__view">
+  <span class="main__img"><img class="main__icon" src={viewsIcon} alt="EyeCon" /></span>
+  <span className="main__views">980,544</span>
+  </div>
+  <div className="main__like">
+    <span className="main__img"><img src={likeIcon} alt="" className="main__heart" /></span>
+  <p className="main__likes">22,479</p>
+  </div>
  </div>
- <p className="main__description">{props[0].description}</p>
 </section>
+<p className="main__description">{props[0].description}</p>
   </>
  )
 }
