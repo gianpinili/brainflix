5aec8e68f9b3c3fe4f71f6dfa4f9dad767cb24d7
Ian Pinili <gianpinili@gmail.com>
2024-02-21 19:38:19 -0800

    add header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,21 @@
 import './Header.scss';
 import mainLogo from '../../assets/logo/BrainFlix-logo.svg'
+import headerIcon from '../../assets/images/Mohan-muruge.jpg'
 
 function Header () {
  return (
-  <header>
+  <header className="header">
    <div className="header__logo">
     <img src={mainLogo} alt="BrainFlix Logo" />
    </div>
    <div className="header__nav">
-    {/* SEARCH INPUT */}
-     <input className="header__input" type="text" placeholder='Search with icon' />
-    {/* UPLOAD BUTTON */}
-    <button className="header__button">Upload (with Icon)</button>
-     {/* ICONS */}
-    <img className="header__icon" src="https://placehold.co/50x50/000000/FFFFFF/png" alt="" />
+     <input className="header__input" type="text" placeholder='Search' />
+     <img className="header-icon__mobile" src={headerIcon} alt="" />
+     </div>
+
+   <div className="header__buttons">
+    <button className="header__button">Upload</button>
+    <img className="header-icon__desktop" src={headerIcon} alt="" />
 
    </div>
   </header>
