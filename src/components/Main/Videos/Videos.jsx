ca01a2df796850e903bf378fa149574aa7daceaa
Ian Pinili <gianpinili@gmail.com>
2024-02-22 19:25:39 -0800

    fix Main Comments/CommentsForm/Videos tablet/desktop styling

--- src/components/Main/Videos/Videos.jsx
+++ src/components/Main/Videos/Videos.jsx
@@ -8,8 +8,8 @@ function Videos ({props}) {
    <div className="videos__image"><img className='videos__img' src="https://placehold.co/100x70" alt="" />
    </div>
    <div className="videos__info">
-    <p className="videos__title">{props[0].title}</p>
-    <p className="videos__channel">{props[0].channel}</p>
+    <p className="videos__title">Exploring Cities of Europe</p>
+    <p className="videos__channel">Aiden Thompson</p>
    </div>
   </div>
   </>
