15132f811692f268e20be579f8700932a2e06f57
Ian Pinili <gianpinili@gmail.com>
2024-02-26 19:04:49 -0800

    add upload page tablet styling

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -13,29 +13,30 @@ function UploadForm() {
 
   return (
     <form onSubmit={handleSubmit} className="upload__form">
-      <div className="upload__thumbnail">
-        <p className="upload__subtitle">Video Thumbnail</p>
-        <img
-          className="upload__img"
-          src="https://placehold.co/290x200"
-          alt=""
-        />
+      <div className="upload__content">
+        <div className="upload__thumbnail">
+          <p className="upload__subtitle">Video Thumbnail</p>
+          <img
+            className="upload__img"
+            src="https://placehold.co/290x200"
+            alt=""
+          />
+        </div>
+        <div className="upload__info">
+          <p className="upload__subtitle">Title your video</p>
+          <input
+            className="upload__input"
+            type="text"
+            placeholder="Add a title to your video"
+          />
+          <p className="upload__subtitle">Add a video description</p>
+          <textarea
+            className="upload__input upload__input--description"
+            type="text"
+            placeholder="Add a description to your video"
+          />
+        </div>
       </div>
-      <div className="upload__info">
-        <p className="upload__subtitle">Title your video</p>
-        <input
-          className="upload__input"
-          type="text"
-          placeholder="Add a title to your video"
-        />
-        <p className="upload__subtitle">Add a video description</p>
-        <textarea
-          className="upload__input upload__input--description"
-          type="text"
-          placeholder="Add a description to your video"
-        />
-      </div>
-
       <UploadButtons />
     </form>
   );
